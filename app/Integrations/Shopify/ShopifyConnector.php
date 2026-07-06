a299460fb0320ef3e1a9a33a939bd9962d3920c1
simonlaszlo <xbeanmanx@protonmail.com>
2026-07-06 12:59:22 +0300

    Scaffold Shopify integration classes

---
+++ app/Integrations/Shopify/ShopifyConnector.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Integrations\Shopify;
+
+class ShopifyConnector
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
