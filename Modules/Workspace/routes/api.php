00884b375de1901309efed9c2f6947c55017e2bd
unteajessica <untea.jessica08@gmail.com>
2026-07-07 17:09:54 +0300

    add tasks api routes

--- Modules/Workspace/routes/api.php
+++ Modules/Workspace/routes/api.php
@@ -4,6 +4,7 @@ use Modules\Workspace\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 use Modules\Workspace\Http\Controllers\WorkspaceController;
 use Modules\Workspace\Http\Controllers\ProjectController;
+use Modules\Workspace\Http\Controllers\TaskController;
 
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
@@ -23,6 +24,22 @@ Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
         Route::get('projects/{projectId}', [ProjectController::class, 'show']);
         Route::put('projects/{projectId}', [ProjectController::class, 'update']);
         Route::delete('projects/{projectId}', [ProjectController::class, 'destroy']);
+
+        Route::get('tasks', [TaskController::class, 'index']);
+        Route::post('tasks', [TaskController::class, 'store']);
+        Route::get('tasks/{taskId}', [TaskController::class, 'show']);
+        Route::put('tasks/{taskId}', [TaskController::class, 'update']);
+        Route::delete('tasks/{taskId}', [TaskController::class, 'destroy']);
+
+        Route::post(
+            'tasks/{taskId}/assignees',
+            [TaskController::class, 'assignEmployees']
+        );
+
+        Route::delete(
+            'tasks/{taskId}/assignees/{employeeId}',
+            [TaskController::class, 'removeEmployee']
+        );
     });
     Route::apiResource('workspaces', WorkspaceController::class)->names('workspace');
 });
