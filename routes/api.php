2ca5f1fe76f0736a4d6c84f456c2a3dd3de8c9db
chisroxana31 <roxana.chis@dev.aico>
2026-07-07 17:30:57 +0300

    feat: implement workspace tasks and time tracking endpoints

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,24 @@
 use App\Http\Controllers\Auth\TokenController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Modules\Workspace\Http\Controllers\AuthController;
+use Modules\Workspace\Http\Controllers\TimeTrackingController;
+
+Route::post('v1/workspace/auth/login', [AuthController::class, 'login']);
 
 Route::prefix('auth')->group(function () {
     Route::post('login', [TokenController::class, 'store'])->middleware('throttle:auth');
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::delete('logout', [TokenController::class, 'destroy']);
+    });
+
+    Route::get('profile', [AuthController::class, 'profile']);
+
+    Route::prefix('tasks/{taskId}')->group(function (): void {
+        Route::post('time-entries/start', [TimeTrackingController::class, 'start']);
+        Route::post('time-entries/{entryId}/stop', [TimeTrackingController::class, 'stop']);
+        Route::get('time-entries', [TimeTrackingController::class, 'index']);
     });
 });
 
